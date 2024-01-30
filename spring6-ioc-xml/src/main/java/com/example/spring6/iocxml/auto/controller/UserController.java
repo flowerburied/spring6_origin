69fcc6044a8aade258319ded4e04ed810bd0e7c1
flowerburied <2842511561@qq.com>
2024-01-30 16:38:56 +0800

    36 auto assemble native

--- spring6-ioc-xml/src/main/java/com/example/spring6/iocxml/auto/controller/UserController.java
+++ spring6-ioc-xml/src/main/java/com/example/spring6/iocxml/auto/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.example.spring6.iocxml.auto.controller;
 
+import com.example.spring6.iocxml.auto.service.impl.UserServiceImpl;
+
 public class UserController {
+
+    public void addUser(){
+        System.out.println("UserController执行。。。");
+//        UserServiceImpl userService = new UserServiceImpl();
+//        userService.addUserService();
+    }
+
 }
